326bceca93b57d2061ced19c0d7ae3dba1941969
Filipe Santos <flip.b.santos@gmail.com>
2016-09-28 18:59:56 -0300

    Changes in the es-linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,8 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    'semi': 0,
+    'no-new': 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
