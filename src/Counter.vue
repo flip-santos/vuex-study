db3111fe9773469da172971eaeaa9d8a25ae8c07
Filipe Santos <flip.b.santos@gmail.com>
2016-09-28 16:38:36 -0300

    Adding first component

---
+++ src/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="counter">
+    {{currentCount}}
+    <button @click="decrement(counterId)">Decrement</button>
+    <button @click="increment(counterId)">Increment</button>
+    <button @click="incrementIfOdd(counterId)">Increment if Odd</button>
+  </div>
+</template>
+
+<script>
+  import { increment, decrement, incrementIfOdd } from './actions';
+
+  export default {
+    props: ['counterId'],
+    vuex: { 
+      state: {
+        currentCount: function(state) {
+          return state.counter[this.counterId];
+        }
+      },
+      actions: {
+        increment,
+        decrement,
+        incrementIfOdd
+      }
+    }
+  }
+</script>
