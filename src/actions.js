61ad75499ba5c21247562f10c6f22ee1ed6c3324
Filipe Santos <flip.b.santos@gmail.com>
2016-09-28 16:34:05 -0300

    Adding first actions

---
+++ src/actions.js
@@ -0,0 +1,9 @@
+export const increment = ({dispach}, counterId) => dispach('INCREMENT', counterId);
+
+export const decrement = ({dispach}, counterId) => dispach('DECREMENT', counterId);
+
+export const addCounter = (store, counterId) => {
+  if(store.state.counters[counterId] % 2 !== 0) {
+    store.dispach('INCREMENT', counterId)
+  }
+}
